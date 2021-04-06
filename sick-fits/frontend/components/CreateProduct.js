b81270dec1d63ded2fb8009a012d1360d0cf4768
Brendan Gassler <bgassler4@gmail.com>
2021-04-06 17:53:40 +1000

    add more inputs to form and form styles

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,43 +1,70 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
+    image: '',
     name: 'Nice Shoes',
     price: 4432,
     description: 'these shoes are great',
   });
   return (
     <div>
-      <form>
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="name"
-            value={inputs.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="price">
-          Name
-          <input
-            type="number"
-            id="price"
-            name="price"
-            placeholder="price"
-            value={inputs.price}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="button" onClick={clearForm}>
-          Clear Form
-        </button>
-        <button type="button" onClick={resetForm}>
-          Reset Form
-        </button>
-      </form>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(inputs);
+        }}
+      >
+        <fieldset>
+          <label htmlFor="image">
+            Name
+            <input
+              type="file"
+              required
+              id="image"
+              name="image"
+              value={inputs.image}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="name"
+              value={inputs.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="price"
+              value={inputs.price}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            Description
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Description"
+              value={inputs.description}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit" onClick={clearForm}>
+            + Add Product
+          </button>
+        </fieldset>
+      </Form>
     </div>
   );
 }

d26583ab025e16cd5ecc991e80740aa862a623a1
Brendan Gassler <bgassler4@gmail.com>
2021-04-13 16:00:13 +1000

    bug fix for create products

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,11 +1,12 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
-import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are getting passed in? And What types are they
     $name: String!
     $description: String!
     $price: Int!
@@ -27,81 +28,77 @@ const CREATE_PRODUCT_MUTATION = gql`
     }
   }
 `;
+
 export default function CreateProduct() {
-  const { inputs, handleChange, resetForm, clearForm } = useForm({
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
     image: '',
     name: 'Nice Shoes',
-    price: 4432,
-    description: 'these shoes are great',
+    price: 34234,
+    description: 'These are the best shoes!',
   });
-
   const [createProduct, { loading, error, data }] = useMutation(
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
     }
   );
-
   return (
-    <div>
-      <Form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          console.log(inputs);
-          // submit input fields to the backend
-          const res = await createProduct();
-          clearForm();
-        }}
-      >
-        <DisplayError error={error} />
-        <fieldset disabled={loading} aria-busy={loading}>
-          <label htmlFor="image">
-            Photo
-            <input
-              type="file"
-              required
-              id="image"
-              name="image"
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="name"
-              value={inputs.name}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              id="price"
-              name="price"
-              placeholder="price"
-              value={inputs.price}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="description">
-            Description
-            <textarea
-              id="description"
-              name="description"
-              placeholder="Description"
-              value={inputs.description}
-              onChange={handleChange}
-            />
-          </label>
-          <button type="submit" onClick={clearForm}>
-            + Add Product
-          </button>
-        </fieldset>
-      </Form>
-    </div>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log(inputs);
+        // Submit the inputfields to the backend:
+        await createProduct();
+        clearForm();
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
